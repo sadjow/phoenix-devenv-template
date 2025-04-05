4d44dd98c17223c7e6d76103b4cc639d3d65a05b
Sadjow Leão <sadjow@gmail.com>
2025-04-05 10:59:01 -0300

    feat: configure markdownlint on the pre-commit

--- devenv.nix
+++ devenv.nix
@@ -1,14 +1,38 @@
 { pkgs, ... }:
 
 {
+  packages = [
+    pkgs.pre-commit
+  ];
+
   languages.elixir = {
     enable = true;
     package = pkgs.beam.packages.erlang_27.elixir_1_18 or pkgs.elixir;
   };
   languages.erlang.enable = true;
 
+  pre-commit = {
+    hooks = {
+      markdownlint = {
+        enable = true;
+        # See more at configuration options: https://github.com/DavidAnson/markdownlint/blob/main/schema/.markdownlint.jsonc
+        settings.configuration = {
+          default = true;
+          MD013 = false;
+          MD024 = false;
+          MD033 = false;
+        };
+      };
+    };
+  };
+
   enterShell = ''
     echo "Elixir version: $(elixir --version)"
     echo "Erlang version: $(erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' -noshell)"
+
+    if [ ! -f .git/hooks/pre-commit ]; then
+      echo "Installing pre-commit hooks..."
+      pre-commit install
+    fi
   '';
 }
